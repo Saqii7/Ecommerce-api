c62e5612cb4dbb771a4ac51cb180b654be151e06
Saqii7 <Saqibchaudhry2000@gmail.com>
2023-12-20 19:46:23 +0500

    Changes

--- app.js
+++ app.js
@@ -4,7 +4,9 @@ const userRoutes = require('./routes/userRoutes')
 
 const app =express();
 app.use(express.json())
-
+app.use('/',(req,res)=>{
+    res.json({message:"Hello From Express JS"})
+})
 app.use('/api/v1',userRoutes)
 
 module.exports = app;
