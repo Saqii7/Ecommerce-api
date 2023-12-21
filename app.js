97f79eae41b293f8110c3f0a8faee08c133ed58e
Saqii7 <Saqibchaudhry2000@gmail.com>
2023-12-21 14:56:32 +0500

    Routes Fixed

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const userRoutes = require('./routes/userRoutes')
+const userRoute = require('./routes/userRoutes')
 
-const app =express();
+const app = express();
 app.use(express.json())
-app.use('/',(req,res)=>{
-    res.json({message:"Hello From Express JS"})
-})
-app.use('/api/v1',userRoutes)
+// app.use('/',(req,res)=>{
+//     res.json({message:"Hello From Express JS"})
+// })
+app.use('/api/v1',userRoute)
 
 module.exports = app;
